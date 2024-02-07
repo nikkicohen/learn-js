11168dd4f32f60da5e3b38093bfd958c05adda7b
nikkicohen <cohen.nik@northeastern.edu>
2024-02-07 12:36:56 -0500

    in class activity 02/06

--- scripts/math.js
+++ scripts/math.js
@@ -2,11 +2,11 @@
 
 // Function to add two numbers
 export function add(a, b) {
-    return a + b;
-  }
+  return a + b;
+}
   
 // Function to multiply two numbers
 export function multiply(a, b) {
-return a * b;
+  return a * b;
 }
   
